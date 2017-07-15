fdb1d047c2c821c7d5f470caed1abbd99b336f46
Venkat453 <venkat.kareti@outlook.com>
2017-07-15 15:46:30 +0530

    TDF

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule }                     from '@angular/core';
+import { RouterModule, Routes }         from '@angular/router';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HerodetailComponent }  from './hero-detail/hero-detail.component';
-import { ProductComponent }     from './product/product.component';
-import { TestComponent }        from './test/test.component';
+import { DashboardComponent }           from './dashboard/dashboard.component';
+import { HeroesComponent }              from './heroes/heroes.component';
+import { HerodetailComponent }          from './hero-detail/hero-detail.component';
+import { ProductComponent }             from './product/product.component';
+import { TestComponent }                from './test/test.component';
+import { FormTemplateDrivenComponent }  from './form-template-driven/form-template-driven.component';
+import { FormModelDrivenComponent }  from './form-model-driven/form-model-driven.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -13,7 +15,9 @@ const routes: Routes = [
   { path: 'detail/:id', component: HerodetailComponent },
   { path: 'heroes',     component: HeroesComponent },
   { path: 'product',    component: ProductComponent },
-  { path: 'test',       component: TestComponent }
+  { path: 'test',       component: TestComponent },
+  { path: 'tempDriven', component: FormTemplateDrivenComponent },
+  { path: 'modelDriven',component: FormModelDrivenComponent }
 ];
 
 @NgModule({
