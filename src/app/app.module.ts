760205ec7fb72d97cdb8a45328f4cb4adef2b9c6
Venkat453 <venkat.kareti@outlook.com>
2017-07-19 14:47:56 +0530

    reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,36 @@
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
-import { FormsModule }          from '@angular/forms';
-import { RouterModule }         from '@angular/router';
-import { HttpModule }           from '@angular/http';
+import { NgModule }                           from '@angular/core';
+import { BrowserModule }                      from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { RouterModule }                       from '@angular/router';
+import { HttpModule }                         from '@angular/http';
 
-import { AppRoutingModule }     from './app-routing.module';
-import { NavBarComponent }      from './layout-pannels/nav-bar/nav-bar.component';
-import { SideBarComponent }     from './layout-pannels/side-bar/side-bar.component';
-import { FooterBarComponent }   from './layout-pannels/footer-bar/footer-bar.component';
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HerodetailComponent }  from './hero-detail/hero-detail.component';
-import { ProductComponent }     from './product/product.component';
-import { TestComponent }        from './test/test.component';
+import { AppRoutingModule }                   from './app-routing.module';
+import { NavBarComponent }                    from './layout-pannels/nav-bar/nav-bar.component';
+import { SideBarComponent }                   from './layout-pannels/side-bar/side-bar.component';
+import { FooterBarComponent }                 from './layout-pannels/footer-bar/footer-bar.component';
+import { AppComponent }                       from './app.component';
+import { DashboardComponent }                 from './dashboard/dashboard.component';
+import { HeroesComponent }                    from './heroes/heroes.component';
+import { HerodetailComponent }                from './hero-detail/hero-detail.component';
+import { ProductComponent }                   from './product/product.component';
+import { TestComponent }                      from './test/test.component';
+import { FormTemplateDrivenComponent }        from './form-template-driven/form-template-driven.component';
+import { FormModelDrivenComponent }           from './form-model-driven/form-model-driven.component';
 
-import { HeroService }          from './service/hero.service';
-import { ProductService }       from './service/product.service';
-import { FormTemplateDrivenComponent } from './form-template-driven/form-template-driven.component';
-import { FormModelDrivenComponent } from './form-model-driven/form-model-driven.component';
+import { HeroService }                        from './service/hero.service';
+import { ProductService }                     from './service/product.service';
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent, DashboardComponent, HeroesComponent, HerodetailComponent,
-    ProductComponent, TestComponent, NavBarComponent, SideBarComponent, FooterBarComponent, FormTemplateDrivenComponent, FormModelDrivenComponent
-  ],
   imports: [
-    BrowserModule, FormsModule, AppRoutingModule, HttpModule,
+    BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule, HttpModule,
     // InMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
+  declarations: [ 
+    AppComponent, DashboardComponent, HeroesComponent, HerodetailComponent,
+    ProductComponent, TestComponent, NavBarComponent, SideBarComponent, FooterBarComponent, 
+    FormTemplateDrivenComponent, FormModelDrivenComponent
   ],
   bootstrap: [AppComponent, ProductComponent],
   providers: [HeroService, ProductService]
